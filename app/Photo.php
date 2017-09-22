b6e6ec5086fb584853d87c31ff829d06db939f94
NifeCZ <karel.sikyr@gmail.com>
2017-09-22 18:20:54 +0200

    users able to something wtihout photos

--- app/Photo.php
+++ app/Photo.php
@@ -4,8 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Role extends user
+class Photo extends Model
 {
-
-
+    //
 }
--- app/User.php
+++ app/User.php
@@ -12,7 +12,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'password', 'role_id', 'name', 'is_active', 'photo_id','file'
     ];
 
     /**
@@ -23,4 +23,15 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+
+
+
+	public function role(){
+
+		return $this->belongsTo('App\Role');
+
+	}
+
+
 }
